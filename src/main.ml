2cf0f46b1deccd73cbd581aac67bc6594439ba31
ohadrau <ohadrau@yahoo.com>
2017-06-01 13:04:42 +0000

    Add type pretty-printing

--- src/main.ml
+++ src/main.ml
@@ -10,4 +10,19 @@ let () =
       print_endline tok;
       raise exn in
   print_endline @@ "AST:\n" ^ string_of_ast ast;
-  print_endline @@ "Output:\n" ^ emit (make_env Filename.(Sys.argv.(1) |> basename |> chop_extension)) ast
+  print_endline @@ "Output:\n" ^ emit (make_env Filename.(Sys.argv.(1) |> basename |> chop_extension)) ast;
+  print_endline @@ "Type:\n" ^ Type.(begin
+    string_of_type @@
+      TQuant (0, Some (LChoose [ "Start", LApp ("()", []),
+                                     LOffer [ "Ok", LApp ("()", []), LApp ("Eps", [])
+                                            ; "Err", LApp ("()", [LApp ("String", [])]), LApp ("Eps", [])
+                                            ]
+                               ]),
+        TRec (1,
+          TImplicit ("client", TVar 0,
+            TOffer ("client", [ "Start", TApp ("()", []),
+              TChoose ("client", [ "Ok", TApp ("()", []), TVar 1
+                                 ; "Err", TApp ("()", [TApp ("String", [])]), TVar 1
+                                 ])
+                              ]))))
+  end)
--- src/type.ml
+++ src/type.ml
@@ -2,10 +2,10 @@ type t =
   | TVar of int
   | TApp of string * t list
   | TArrow of t * t
-  | TQuant of string * limit option
-  | TImplicit of string * t
+  | TQuant of int * limit option * t
+  | TImplicit of string * t * t
   | TNamed of string * t
-  | TRec of string * t
+  | TRec of int * t
   | TOffer of string * (string * t * t) list
   | TChoose of string * (string * t * t) list
 
@@ -14,5 +14,31 @@ and limit =
   | LApp of string * limit list
   | LArrow of limit * limit
   | LRec of string * limit
-  | LOffer of string * (string * limit * limit) list
-  | LChoose of string * (string * limit * limit) list
+  | LOffer of (string * limit * limit) list
+  | LChoose of (string * limit * limit) list
+
+let rec string_of_type = function
+  | TVar i -> "_" ^ string_of_int i
+  | TApp (t, args) -> t ^ "(" ^ String.concat ", " (List.map string_of_type args) ^ ")"
+  | TArrow (t, u) -> string_of_type t ^ " -> " ^ string_of_type u
+  | TQuant (v, None, t) -> "forall _" ^ string_of_int v ^ ". " ^ string_of_type t
+  | TQuant (v, Some l, t) -> "forall _" ^ string_of_int v ^ " < ..." ^ string_of_limit l ^ ". " ^ string_of_type t
+  | TImplicit (s, t, u) -> "[" ^ s ^ " : " ^ string_of_type t ^ "] " ^ string_of_type u
+  | TNamed (s, t) -> "(" ^ s ^ " : " ^ string_of_type t ^ ")"
+  | TRec (v, t) -> "rec _" ^ string_of_int v ^ ". " ^ string_of_type t
+  | TOffer (w, offers) -> "?" ^ w ^ "{" ^ String.concat ", " (List.map string_of_toffer offers) ^ "}"
+  | TChoose (w, offers) -> "!" ^ w ^ "{" ^ String.concat ", " (List.map string_of_toffer offers) ^ "}"
+
+and string_of_limit = function
+  | LVar i -> "_" ^ string_of_int i
+  | LApp (t, args) -> t ^ "(" ^ String.concat ", " (List.map string_of_limit args) ^ ")"
+  | LArrow (t, u) -> string_of_limit t ^ " -> " ^ string_of_limit u
+  | LRec (v, t) -> "rec _" ^ v ^ ". " ^ string_of_limit t
+  | LOffer offers -> "?{" ^ String.concat ", " (List.map string_of_loffer offers) ^ "}"
+  | LChoose offers -> "!{" ^ String.concat ", " (List.map string_of_loffer offers) ^ "}"
+
+and string_of_toffer (msg, t1, t2) =
+  msg ^ string_of_type t1 ^ " ~> " ^ string_of_type t2
+
+and string_of_loffer (msg, l1, l2) =
+  msg ^ string_of_limit l1 ^ " ~> ..." ^ string_of_limit l2
