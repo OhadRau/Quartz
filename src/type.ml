0a8b154c4450f82be082a83f47358dd4ab895406
ohadrau <ohadrau@yahoo.com>
2017-06-11 15:33:10 -0400

    Add Type.{neighbors,binary} for checking type limits

--- src/type.ml
+++ src/type.ml
@@ -46,3 +46,37 @@ and string_of_toffer (msg, t1, t2) =
 
 and string_of_loffer (msg, l1, l2) =
   msg ^ string_of_limit l1 ^ " ~> ..." ^ string_of_limit l2
+
+(* Get the names of all sessions that a type communicates with *)
+let neighbors t =
+  let module S = Set.Make(String) in
+  let fold_set f xs =
+    List.fold_right (fun x s -> S.union s (f x)) xs S.empty in
+  let rec traverse = function
+    | TVar _ -> S.empty
+    | TApp (_, xs) -> fold_set traverse xs
+    | TArrow (xs, y) -> S.union (fold_set traverse xs) (traverse y)
+    | TQuant (_, _, t) -> traverse t
+    | TImplicit (_, t, c) -> S.union (traverse t) (traverse c)
+    | TNamed (_, t) -> traverse t
+    | TRec (_, t) -> traverse t
+    | TOffer (w, offers) | TChoose (w, offers) ->
+      S.add w (fold_set (fun (_, t, c) -> S.union (traverse t) (traverse c)) offers) in
+  traverse t |> S.elements
+
+(* Make a session type binary with respect to the session named w *)
+let rec binary w = function
+  | TVar i -> TVar i
+  | TApp (s, ts) -> TApp (s, List.map (binary w) ts)
+  | TArrow (xs, y) -> TArrow (List.map (binary w) xs, binary w y)
+  | TQuant (i, l, t) -> TQuant (i, l, binary w t)
+  | TImplicit (s, x, y) -> TImplicit (s, binary w x, binary w y)
+  | TNamed (s, t) -> TNamed (s, binary w t)
+  | TRec (i, t) -> TRec (i, binary w t)
+  | TOffer (s, offers) when s = w ->
+    TOffer (w, List.map (fun (n, t, c) -> (n, binary w t, binary w c)) offers)
+  | TChoose (s, offers) when s = w ->
+    TChoose (w, List.map (fun (n, t, c) -> (n, binary w t, binary w c)) offers)
+  (* FIXME: This assumes that each offer provides a unifiable continuation *)
+  | TOffer (_, (_, _, c)::_) | TChoose (_, (_, _, c)::_) -> binary w c
+  | TOffer (_, []) | TChoose (_, []) -> TApp ("End", [])
